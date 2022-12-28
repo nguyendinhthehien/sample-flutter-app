8c2683450c5ef8f2dd69ee9be467dd520d1d754b
hienndt1997@gmail.com <thehien123>
2022-12-28 11:37:27 +0700

    first commit

---
+++ bin/sample_flutter_app.dart
@@ -0,0 +1,5 @@
+import 'package:sample_flutter_app/sample_flutter_app.dart' as sample_flutter_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sample_flutter_app.hello()}!');
+}
---
+++ test/sample_flutter_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:sample_flutter_app/sample_flutter_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
